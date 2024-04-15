a30c3be9da4ea7449b497d14714db044e980c130
Théo Berry <theo.berry.meleck@gmail.com>
2024-04-15 14:20:16 +0200

    Finished the design

---
+++ src/components/DiceButton.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+<template>
+    <div class="dice-container">
+        <img
+            src="/images/icon-dice.svg"
+            alt="Get a random quote" />
+    </div>
+</template>
+<style scoped>
+.dice-container {
+    background-color: hsl(150, 100%, 66%);
+    padding: 20px;
+    border-radius: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: 45px;
+}
+img {
+    width: 25px;
+    height: 25px;
+}
+.dice-container:hover {
+    cursor: pointer;
+    box-shadow: 0px 0px 30px hsl(150, 100%, 66%);
+}
+</style>
---
+++ src/views/Advice.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref, onMounted, onBeforeMount } from 'vue';
+import DiceButton from '../components/DiceButton.vue';
+
+const dividerSrc = ref('');
+
+const updateScreenSize = () => {
+    const screenWidth = window.innerWidth;
+    dividerSrc.value =
+        screenWidth <= 1440
+            ? '/images/pattern-divider-mobile.svg'
+            : '/images/pattern-divider-desktop.svg';
+};
+onMounted(() => {
+    window.addEventListener('resize', updateScreenSize);
+    updateScreenSize();
+});
+onBeforeMount(() => {
+    window.removeEventListener('resize', updateScreenSize);
+});
+</script>
+
+<template>
+    <div class="invisible-div">
+        <section class="advice-container">
+            <h1>h1</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Laudantium perferendis quia
+            </p>
+            <img
+                :src="dividerSrc"
+                alt="divider" />
+            <DiceButton />
+        </section>
+    </div>
+</template>
+
+<style scoped>
+.invisible-div {
+    padding-bottom: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.advice-container {
+    background-color: hsl(217, 19%, 24%);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    padding: 20px 10px 10px 10px;
+    margin: 10px;
+}
+h1 {
+    color: hsl(150, 100%, 66%);
+    font-size: 1rem;
+}
+p {
+    color: hsl(193, 38%, 86%);
+    font-size: 28px;
+    text-align: center;
+}
+@media screen and (min-width: 1440px) {
+    .advice-container {
+        max-width: 45%;
+        padding: 30px 25px 10px 25px;
+    }
+}
+</style>
