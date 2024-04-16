7b0ccc02b18bb6a7e1c3f42b872622451666a373
Théo Berry <theo.berry.meleck@gmail.com>
2024-04-16 10:07:31 +0200

    made changes to fix issues

--- src/components/ApiFetch.vue
+++ src/components/ApiFetch.vue
@@ -1,45 +1,46 @@
 <script setup>
-import { ref, watchEffect, defineProps } from 'vue';
+	import { ref, watch } from 'vue';
 
-const props = defineProps(['clicked']);
-const apiData = ref(null);
+	const props = defineProps(['clicked']);
+	const apiData = ref(null);
 
-const fetchData = async () => {
-    try {
-        const response = await fetch('https://api.adviceslip.com/advice');
-        if (!response.ok) {
-            throw new Error('Error fetching data');
-        }
-        const jsonData = await response.json();
-        apiData.value = jsonData;
-    } catch (error) {
-        console.log(error);
-    }
-};
+	const fetchData = async () => {
+		try {
+			const response = await fetch('https://api.adviceslip.com/advice');
+			if (!response.ok) {
+				throw new Error('Error fetching data');
+			}
+			const jsonData = await response.json();
+			apiData.value = jsonData;
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-watchEffect(() => {
-    fetchData();
-    if (props.clicked) {
-        fetchData();
-    }
-});
+	watch(
+		() => props.clicked,
+		() => {
+			fetchData();
+		},
+		{ immediate: true }
+	);
 </script>
 
 <template>
-    <template v-if="apiData">
-        <h1>Advice #{{ apiData.slip.id }}</h1>
-        <p>"{{ apiData.slip.advice }}"</p>
-    </template>
+	<template v-if="apiData">
+		<h1>Advice #{{ apiData.slip.id }}</h1>
+		<p>"{{ apiData.slip.advice }}"</p>
+	</template>
 </template>
 <style scoped>
-h1 {
-    color: hsl(150, 100%, 66%);
-    font-size: 0.8rem;
-    text-transform: uppercase;
-}
-p {
-    color: hsl(193, 38%, 86%);
-    font-size: 28px;
-    text-align: center;
-}
+	h1 {
+		color: hsl(150, 100%, 66%);
+		font-size: 0.8rem;
+		text-transform: uppercase;
+	}
+	p {
+		color: hsl(193, 38%, 86%);
+		font-size: 28px;
+		text-align: center;
+	}
 </style>
--- src/components/DiceButton.vue
+++ src/components/DiceButton.vue
@@ -1,42 +1,40 @@
 <script setup>
-import { defineEmits } from 'vue';
+	const emit = defineEmits(['click-start, click-end']);
 
-const emit = defineEmits(['click-start, click-end']);
-
-const startClick = () => {
-    emit('click-start');
-};
-const endClick = () => {
-    emit('click-end');
-};
+	const startClick = () => {
+		emit('click-start');
+	};
+	const endClick = () => {
+		emit('click-end');
+	};
 </script>
 <template>
-    <div
-        class="dice-container"
-        @mousedown="startClick"
-        @mouseup="endClick">
-        <img
-            src="/images/icon-dice.svg"
-            alt="Get a random quote" />
-    </div>
+	<div
+		class="dice-container"
+		@mousedown="startClick"
+		@mouseup="endClick">
+		<img
+			src="/images/icon-dice.svg"
+			alt="Get a random quote" />
+	</div>
 </template>
 <style scoped>
-.dice-container {
-    background-color: hsl(150, 100%, 66%);
-    padding: 20px;
-    border-radius: 100px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    top: 45px;
-}
-img {
-    width: 25px;
-    height: 25px;
-}
-.dice-container:hover {
-    cursor: pointer;
-    box-shadow: 0px 0px 30px hsl(150, 100%, 66%);
-}
+	.dice-container {
+		background-color: hsl(150, 100%, 66%);
+		padding: 20px;
+		border-radius: 100px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		top: 45px;
+	}
+	img {
+		width: 25px;
+		height: 25px;
+	}
+	.dice-container:hover {
+		cursor: pointer;
+		box-shadow: 0px 0px 30px hsl(150, 100%, 66%);
+	}
 </style>
